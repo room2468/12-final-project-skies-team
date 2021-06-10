1f32c5c70424857fc7207d11634a067d405f73fa
alfandirio <73221556+alfandirio@users.noreply.github.com>
2021-06-10 10:31:16 +0700

    recomengenre

--- recomendasi.dart
+++ recomendasi.dart
@@ -9,7 +9,8 @@ class Recomendasi extends StatelessWidget {
             new Padding(padding: new EdgeInsets.all(20.0),),
             new Padding(padding: new EdgeInsets.all(20.0),),
             new Icon(Icons.phone_android,size: 90.0, color: Colors.lightBlueAccent,),
-            new Text("PHONE", style: new TextStyle(fontSize: 30.0, color: Colors.lightGreen),)         ],
+            new Text("PHONE", style: new TextStyle(fontSize: 30.0, color: Colors.lightGreen),)        
+             ],
         ),
       ),
     );
