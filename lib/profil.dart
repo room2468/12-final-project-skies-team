d76da1a21291777fd0427e0fb6ff5f7455a784bf
Desy Annisa R <annisaramadhani27@gmail.com>
2021-06-11 01:01:21 +0700

    Halaman Profil Pemain

--- lib/profil.dart
+++ lib/profil.dart
@@ -15,13 +15,53 @@ class Profil extends StatelessWidget {
             ),
             new Icon(
               Icons.photo,
-              size: 90.0,
+              size: 80.0,
               color: Colors.lightBlueAccent,
             ),
             new Text(
               "Profil Pemain",
-              style: new TextStyle(fontSize: 30.0, color: Colors.lightGreen),
-            )
+              style: new TextStyle(fontSize: 20.0, color: Colors.black),
+            ),
+            new Text(
+              "OH! MASTER/n 1.Lee Min-ki/n Tanggal Lahir : 16 Januari 1985/n Tahun Aktif : 2004 - sekarang/n Agensi : Good Management/n 2.Nana (Im Jin-ah)/n Tanggal Lahir : 14 Sepember 1991/n Tahun Aktif : 2009 - sekarang/n Agensi : Pledis Entertainment/n TMI : Nana pernah menjadi bagian dari girlgroup After School dan subunit Orange Caramel/n 3.Kang Minhyuk/n Tanggal Lahir : 28 Juni 1991/n Tahun Aktif : 2010 - sekarang/n Agensi : FNC Entertainment/n TMI : Kang Minhyuk tergabung dalam grup band CNBLUE yang debut pada tahun 2009 dan masih aktif hingga sekarang.",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "CRASH LANDING ON YOU/n 1.HyunBin/n Tanggal Lahir : 25 September 1982/n Tahun Aktif : 2003 - sekarang/n Agensi : VAST Entertainent/n 2.Son Ye-jin/n Tanggal lahir : 11 Januari 1982/n Tahun Aktif : 2000 - sekarang/n Agensi : MSTeam Entertainment/n 3.Kim Jung-hyun/n Tanggal Lahir : 5 April 1990/n Tahun Aktif : 2014 - sekarang/n Agensi : Tidak ada/n TMI : Pada tanggal 12 Mei 2021 Kim Jung-hyun mengakhiri kontrak dengan O& Entertainment./n 4.Seo Ji-hye/n Tanggal Lahir : 24 Agustus 1984/n Tahun Aktif : 2003 - sekaran/n Agensi : Culture Depot",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "FLOWER OF EVIL/n 1.Lee Joon-gi/n Tanggal Lahir : 17 April 1982/n Tahun Aktif : 2001 - sekarang/n Agensi : Namoo Actors/n 2.Moon Chae-won/n Tanggal Lahir : 13 November 1986/n Tahun Aktif : 2007 - sekarang/n Agensi : MSTeam Entertainment/n 3.Jang Hee-jin/n Tanggal Lahir : 9 Mei 1983/n Tahun Aktif : 2003 - sekarang Agensi : Lucky Company",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "EXTRACURRICULAR/n 1.Kim Dong-hee/n Tanggal Lahir : 13 Juni 1999/n Tahun Aktif : 2018 - sekarang/n Agensi : NPIO Entertainment/n 2.Park Ju-hyun/n Tanggal Lahir : 5 Oktober 1994/n Tahun Aktif : 2015 - sekarang/n Agensi : 935 Entertainment/n 3.Jung Da-bin/n Tanggal Lahir : 25 April 2000/n Tahun Aktif : 2003 - sekarang/n Agensi R's Company/n 4.Nam Yoon-soo/n Tanggal Lahir : 14 Juli 1997/n Tahun Aktif : 2014 - sekarang/n Agensi : Agency Garten",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "SWEET HOME/n 1.Song Kang/n Tanggal Lahir : 23 April 1994/n Tahun Aktif : 2017 - sekarang/n Agensi : Namoo Actors/n 2.Lee Jin-wook/n Tanggal Lahir : 16 September 1981/n Tahun Aktif : 2003 - sekarang/n Agensi : Glove Entertainment/n 3.Lee Si-young/n Tanggal Lahir : 17 April 1982/n Tahun Aktif : 2008 - sekarang/n Agensi : Ace Factory",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "THE CALL/n 1.Park Shin-hye/n Tanggal Lahir : 18 Februari 1990/n Tahun Aktif : 2003 - sekarang/n Agensi : S.A.L.T Entertainment/n 2.Jeon Jong-seo/n Tanggal Lahir : 5 Juli 1994/n Tahun Aktif : 2018 - sekarang/n Agensi : United Talent Agency",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "MIRACLE IN CELL NO.7/n 1.Ryu Seung-ryong/n Tanggal Lahir : 29 November 1970/n Tahun Aktif : 2004 - sekarang/n Agensi : Prain TPC/n 2.Kal So-won Tanggal Lahir : 14 Agustus 2006/n Tahun Aktif : 2012 - sekarang/n Agensi : YG Entertainment/n 3.Park Shin-hye/n Tanggal Lahir : 18 Februari 1990/n Tahun Aktif : 2003 - sekarang/n Agensi : S.A.L.T Entertainment",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "MOVE TO HEAVEN/n 1.Lee Je-hoon/n Tanggal Lahir : 4 Juli 1984/n Tahun Aktif : 2006 - sekarang/n Agensi : Tidak ada/n TMI : Pada tanggal 1 April pihak Saram Entertainment mengatakan bahwa Lee jehoon resmi mengakhiri kontrak dengan Saram Entertainment dan akan saling mendukung jalan masing-masing./n 2.Tang Joon-sang/n Tanggal Lahir : 13 Agustus 2003/n Tahun Aktif : 2010 - sekarang/n Agensi : CL& Company",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "SISYPHUS:THE MYTH/n 1.Park Shin-hye/n Tanggal Lahir : 18 Februari 1990/n Tahun Aktif : 2003 - sekarang/n Agensi : S.A.L.T Entertainment/n 2.Cho Seung-woo/n Tanggal Lahir : 28 Maret 1980/n Tahun Aktif : 2000 - sekarang/n Agensi : Good Man Story Entertainment",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "LUCA:THE BEGINNING/n 1.Kim Rae-won/n Tanggal Lahir : 19 Maret 1981/n Tahun Aktif : 1997 - sekarang/n Agensi : HB Entertainment/n 2.Lee Da-hee/n Tanggal Lahir : 15 Maret 1985/n Tahun Aktif : 2002 - sekarang/n Agensi : Huayi Brothers/n 3.Kim Sung-oh/n Tanggal Lahir : 15 September 1978/n Tahun Aktif : 2000 - sekarang/n Agensi : Huayi Brothers",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
           ],
         ),
       ),
