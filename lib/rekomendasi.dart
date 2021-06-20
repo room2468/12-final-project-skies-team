55d2d33a9d8712925120130d9466839352e2b5c7
Desy Annisa R <annisaramadhani27@gmail.com>
2021-06-20 20:59:34 +0700

    rekomendasi judul update

--- lib/rekomendasi.dart
+++ lib/rekomendasi.dart
@@ -14,14 +14,98 @@ class Rekomendasi extends StatelessWidget {
               padding: new EdgeInsets.all(20.0),
             ),
             new Icon(
-              Icons.book_outlined,
-              size: 90.0,
+              Icons.movie_creation,
+              size: 80.0,
               color: Colors.lightBlueAccent,
             ),
             new Text(
-              "Rekomendasi Drama",
-              style: new TextStyle(fontSize: 30.0, color: Colors.lightGreen),
-            )
+              "Rekomendasi Judul Drama/Film",
+              style: new TextStyle(fontSize: 20.0, color: Colors.black),
+            ),
+            new Text(
+              "KOMEDI ROMANTIS : ",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "1. Oh! Master / Oh My Ladylord (2021)",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "2. Crash Landing on You",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "KRIMINAL : ",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "1. Flower of Evil",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "2. Extracurricular",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "THRILLER : ",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "1. Sweet Home",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "2. The Call",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "MELLOW DRAMA : ",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "1. Miracle in Cell No.7",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "2. Move to Heaven",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "ACTIONS : ",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "1. Sisypush: The Myth",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "2. Luca: The Beginning",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "HISTORIKAL (SAEGUK) : ",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "1. Dae Jang Geum",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "2. My Sassy Girl 2017",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "FANTASI : ",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "1. The Uncanny Counter",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
+            new Text(
+              "2. Hi Bye, Mama!",
+              style: new TextStyle(fontSize: 15.0, color: Colors.black),
+            ),
           ],
         ),
       ),
